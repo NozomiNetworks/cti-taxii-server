345d0858a9b8850ef1de962493b2907630560969
corranoz <matteo.corradini@nozominetworks.com>
2025-11-03 08:58:25 +0100

    feat(TIA-292): add tests

--- medallion/filters/mongodb_result_counter.py
+++ medallion/filters/mongodb_result_counter.py
@@ -41,7 +41,7 @@ class MongoDBResultCounter:
         return self.database.objects.count_documents({"_collection_id": collection_id})
 
     def count_first_or_last(self, collection_id: str) -> int:
-        return self.database.collections('manifests').countDocuments({"_collection_id": collection_id})
+        return self.database.collections('manifests').count_documents({"_collection_id": collection_id})
 
     def count_first_and_last(self, collection_id: str) -> int:
         return self.database.collections('manifests').aggregate(
@@ -64,7 +64,7 @@ class MongoDBResultCounter:
         )
 
     def count_specific_dates(self, collection_id: str, actual_dates: List[float]) -> int:
-        return self.database.collections('objects').countDocuments(
+        return self.database.collections('objects').count_documents(
             {
                 "$and": [
                     {"_collection_id": collection_id},
