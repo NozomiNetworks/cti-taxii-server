eafc30e016fb04e26d23b1f044365f44b88b011c
corranoz <matteo.corradini@nozominetworks.com>
2026-04-28 14:23:08 +0200

    feat(TIA-489): tests

--- medallion/test/unit/test_mongodb_next_gen_filter.py
+++ medallion/test/unit/test_mongodb_next_gen_filter.py
@@ -411,12 +411,12 @@ class TestMongoDBNextGenFilterIndexSelection:
         # Mock index_information to return a dict containing the big_cardinality index
         api_root_db.objects.index_information.return_value = {
             "_id_": {"key": [("_id", 1)]},
-            mongodb_nextgen_filter._inverted_index_big_cardinality: {"key": [("_collection_id", 1)]},
+            mongodb_nextgen_filter._inverted_index_big_cardinality: {"key": [("collection_id", 1)]},
         }
 
         pipeline = {
             "pattern": {"$regex": self._build_pattern_regex(IndicatorType.URL)},
-            "_collection_id": {"$eq": "50c8f051-debf-4704-b05c-935d84d38426"},
+            "collection_id": {"$eq": "50c8f051-debf-4704-b05c-935d84d38426"},
         }
 
         mongodb_nextgen_filter._get_sorted_results_with_next_limit_on_objects(pipeline, 10)
